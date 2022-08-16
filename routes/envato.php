ea0d2aa8ac467ef39324e81bb77cfd750b8e15d0
Mohammad Prince <mprince2k16@gmail.com>
2022-08-16 17:56:16 +0600

    route

--- routes/envato.php
+++ routes/envato.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/purchase-code-validation', function () {
-    return view('index'); 
+    return view('envato-license-verification::index');
 });
