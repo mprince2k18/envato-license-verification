1bfb139a0173827b68a5bb83aaae279e3762abac
Mohammad Prince <mprince2k16@gmail.com>
2022-08-18 13:24:42 +0600

    welcome page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,27 +1,87 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ __('Purchase Code Verification') }}</title>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Enter Your Information & Verify The Application</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/7.css">
-</head>
-<body>
+  </head>
 
-    <div class="window" style="max-width: 300px">
-        <div class="title-bar">
-            <div class="title-bar-text">A window with contents</div>
-            <div class="title-bar-controls">
-            <button aria-label="Minimize"></button>
-            <button aria-label="Maximize"></button>
-            <button aria-label="Close"></button>
+    <style>
+        .violet::before,
+        .violet > .title-bar {
+            background-color: #805ba5;
+        }
+
+        .invalid{
+            color: red; 
+            font-size: 12px;
+        }
+
+        .h-100vh{
+            height: 100vh;
+        }
+
+        .main-bg{
+            background-color: #f1f2f6;
+        }
+
+        .flex-end{
+            justify-content: flex-end
+        }
+    </style>
+
+  <body>
+
+    <!-- center div -->
+    <div class="container-fluid main-bg">
+      <div class="row justify-content-center align-items-center h-100vh">
+        <div class="col-md-4">
+            <div class="window glass violet">
+                <div class="title-bar">
+                    <div class="title-bar-text">Enter Your Information & Verify The Application</div>
+                    <div class="title-bar-controls">
+                        <button aria-label="Close"></button>
+                    </div>
+                </div>
+                <div class="window-body">
+                    
+                    <div class="field-row-stacked">
+                        <label for="text22">Enter Your Envato Name</label>
+                        <input id="text22" type="text">
+                        <div class="title-bar-text invalid">Please enter your name</div>
+                    </div>
+                    
+                    <div class="field-row-stacked">
+                        <label for="text23">Enter Your Envato Email Address</label>
+                        <input id="text23" type="text">
+                        <div class="title-bar-text invalid">Please enter email address</div>
+                    </div>
+
+                    <div class="field-row-stacked">
+                        <label for="text23">Enter Your Envato Username</label>
+                        <input id="text23" type="text">
+                        <div class="title-bar-text invalid">Please enter envato username</div>
+                    </div>
+
+                    <div class="field-row-stacked">
+                        <label for="text23">Enter Your Application Purchase Code</label>
+                        <input id="text23" type="text">
+                        <div class="title-bar-text invalid">Please enter application purchase code</div>
+                    </div>
+
+                    <section class="field-row flex-end">
+                        <button>Submit</button>
+                        <button>Reset</button>
+                    </section>
+
+                </div>
             </div>
         </div>
-        <div class="window-body">
-            <p>There's so much room for activities!</p>
-        </div>
+      </div>
     </div>
-    
-</body>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
 </html>
